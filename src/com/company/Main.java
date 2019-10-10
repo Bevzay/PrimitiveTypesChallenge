1927fddddfcdb0a254b4fd5de305e7bc10561c69
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-10 18:13:00 +1300

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+
+        System.out.println(longTotal);
+
+        short shortTotal = (short)(1000 + 10 * (byteValue + shortValue + intValue));
+        // needs to be casted else it will give an error
+
     }
 }
